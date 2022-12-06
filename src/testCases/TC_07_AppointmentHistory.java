8aaace7342436434354366b5365c2287c6f4a132
jayesh <jayesh.pansare.l@gmail.com>
2022-12-06 15:49:29 +0530

    patients, users and appointments page

---
+++ src/testCases/TC_07_AppointmentHistory.java
@@ -0,0 +1,44 @@
+package testCases;
+
+import lib.BaseClass;
+import lib.XLUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import stepDefination.AdminAppointmentPage;
+import stepDefination.AdminLoginPage;
+
+import java.io.IOException;
+
+public class TC_07_AppointmentHistory extends BaseClass {
+    static String fileURLPath = "";
+    static{
+        String projectPath = System.getProperty("user.dir");
+        String fileURL = projectPath+"\\src\\data\\login.xlsx";
+        fileURLPath = fileURL;
+    }
+
+    private static final Logger log = LogManager.getLogger(TC_02_validateAdminLoginPage.class);
+
+    @Test(priority=0, groups="", dataProvider = "loginValidDataProvider")
+    public void validateValidLoginCreds(String username, String password){
+        AdminLoginPage adminLoginPageObj = new AdminLoginPage(getDriver());
+        adminLoginPageObj.clickOnAdminBtn();
+        log.info("Open Login page");
+        adminLoginPageObj.validateLoginCreds(username, password);
+        adminLoginPageObj.clickOnLoginBtn();
+    }
+
+    @Test(priority=1, groups="")
+    public void openPatientsSubMenu(){
+        AdminAppointmentPage objAdminAppointmentPage = new AdminAppointmentPage(getDriver());
+        objAdminAppointmentPage.openAppointmentMenu();
+    }
+
+    @DataProvider(name = "loginValidDataProvider")
+    public Object[][] loginValidDataProviderMethod() throws IOException {
+        XLUtils obj = new XLUtils();
+        return new Object[][] {{obj.getCellData(fileURLPath, "loginValid", 1,1), obj.getCellData(fileURLPath, "loginValid", 1,2)}};
+    }
+}
--- src/testCases/test.java
+++ src/testCases/test.java
@@ -1,7 +1,21 @@
 package testCases;
 
+import cucumber.api.java.hu.Ha;
+
+import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class test {
     public static void main(String[] args){
+        //list1 = {1,2,3,11} and list2 = {11,33,3,4}
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("11");
+
 
     }
 }
